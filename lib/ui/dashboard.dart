5cb8543f4ddc3c6161b1515ad6e35d170788c425
rehotechgh <89611715+rehotechgh@users.noreply.github.com>
2024-10-04 10:23:19 -0700

    card info

--- lib/ui/dashboard.dart
+++ lib/ui/dashboard.dart
@@ -9,6 +9,59 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar:AppBar(
+        backgroundColor: const Color(0xffcdd7fc),
+      ),
+      body: Padding(
+        padding:const EdgeInsets.all(15),
+        child:Column(
+            children:[
+              Container(
+
+                width: MediaQuery.of(context).size.width,
+                height: 110,
+
+                decoration: BoxDecoration(
+                  color: const Color(0xffcdd7fc),
+                  borderRadius: BorderRadius.circular(20),
+               // Curved edges
+                ),
+                child:const Padding(
+                  padding:EdgeInsets.all(10),
+                  child: Row(
+                    children:[
+                      CircleAvatar(
+
+                        radius: 40, // Radius of the avatar
+                          backgroundImage: AssetImage("images/kojo.png"),
+                        // You can use NetworkImage for images from the internet
+                      ),
+                      SizedBox(width:10),
+                     Padding(
+                       padding: EdgeInsets.all(20.0),
+                       child: Column(
+
+
+                         children:[
+                           Text('Francis Kojo Asare Yawson'),
+                           Text('GHC 56,000.00',   style: TextStyle(
+                             fontWeight: FontWeight.bold,
+                             fontSize: 18, // You can adjust the font size as needed
+                           ),),
+
+                         ],
+                       ),
+                     ),
+
+
+                    ]
+                  )
+                ),
+              ),
+
+            ]),
+      ),
+    );
   }
 }
